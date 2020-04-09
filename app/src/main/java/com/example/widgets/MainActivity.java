f5e8232d3e911dac8924c071fb4c7a0864ac9160
Klara Frommelin <a19klafr@student.his.se>
2020-04-09 12:05:01 +0200

    arraylists and adapter had been added and connected. Toast for list-items added

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
+    private String[] mntLocations = {"Alps","Alps","Alaska"};
+    private int[] mntHeights = {4478,4808,6190};
+
+    private ArrayList<String> listData = new ArrayList<>(Arrays.asList(mountainNames));
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_items,R.id.my_list_items,listData);
+        ListView My_ListView = (ListView) findViewById(R.id.my_listview);
+        My_ListView.setAdapter(adapter);
+        My_ListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                Toast.makeText(getApplicationContext(),"Mountaintop is located in: "+mntLocations[i],Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
